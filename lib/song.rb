09999d26c77e71b04e1425e511ba53eb707ebf24
Ebony Sharrock <ebonysharrock@gmail.com>
2019-11-23 02:32:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,31 @@
 class Song
   @@count = 0
-attr_accessor :name, :artists, :genres, :count
-def initialize
+  @@artists = []
+  @@genres = []
+def count
+    @@count
+end
+def initialize(name,artist,genre)
   @@count+=1
+  @@artists << artist
+  @@genres << genre
+  @name=name
+  @artist=artist
+  @genre=genre
+end
+def name
+  @name
+end
+def artist
+  @artist
+end
+def genre
+  @genre
 end
 def genre_count
   {}
 end
 def artist_count
-  {}
+{}
 end
 end
